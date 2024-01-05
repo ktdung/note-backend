19f9c0c57ba677eb8b48d0de6b879723c3a7f7a4
ktdung <ktdunglibre@gmail.com>
2024-01-05 09:44:18 +0700

    add express.static

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('dist'));
 
 let notes = [
   {
@@ -31,10 +32,6 @@ const generateId = () => {
 
   return maxId + 1;
 };
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>');
-});
 
 app.get('/api/notes', (req, res) => {
   const note = req.body;
