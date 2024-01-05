e56d55bdb3e6f50c25e3b1bf0a9c1154c66696e8
ktdung <ktdunglibre@gmail.com>
2024-01-05 10:45:26 +0700

    uibuild

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
+// const http = require('http');
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const app = express();
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method);
-  console.log('Path:  ', request.path);
-  console.log('Body:  ', request.body);
-  console.log('---');
-  next();
-};
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
-
-app.use(cors());
 app.use(express.json());
-app.use(requestLogger);
-app.use(express.static('build'));
+app.use(cors());
+app.use(express.static('dist'));
 
 let notes = [
   {
@@ -38,62 +26,58 @@ let notes = [
   },
 ];
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
-});
-
-app.get('/api/notes', (req, res) => {
-  res.json(notes);
-});
-
 const generateId = () => {
   const maxId =
     notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+
   return maxId + 1;
 };
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body;
+app.get('/api/notes', (req, res) => {
+  const note = req.body;
+  console.log(note);
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  const body = req.body;
+  console.log(body);
 
   if (!body.content) {
-    return response.status(400).json({
+    return res.status(400).json({
       error: 'content missing',
     });
   }
 
   const note = {
     content: body.content,
-    important: body.important || false,
-    date: new Date(),
+    important: Boolean(body.important) || false,
     id: generateId(),
   };
 
   notes = notes.concat(note);
 
-  response.json(note);
+  res.json(note);
 });
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+
+  const note = notes.find((note) => note.id === Number(id));
 
   if (note) {
-    response.json(note);
+    res.json(note);
   } else {
-    response.status(404).end();
+    res.status(404).end();
   }
-
-  response.json(note);
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.find((note) => note.id !== id);
 
-  response.status(204).end();
+  res.status(204).end();
 });
-
-app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
