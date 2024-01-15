96a4412e1f24cc2cadd28453cdc4d82568b0d162
ktdung <ktdunglibre@gmail.com>
2024-01-15 13:50:43 +0700

    add jest test

--- utils/middleware.js
+++ utils/middleware.js
@@ -17,7 +17,7 @@ const errorHandler = (error, req, res, next) => {
 
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' });
-  } else {
+  } else if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message });
   }
   next(error);
