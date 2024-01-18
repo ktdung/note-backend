6e390f5897301593223794ca242702d65c797a8c
ktdung <ktdunglibre@gmail.com>
2024-01-18 09:51:14 +0700

    test async router backend

--- controllers/notes.controller.js
+++ controllers/notes.controller.js
@@ -6,39 +6,40 @@ notesRouter.get('/', async (req, res) => {
   res.json(notes);
 });
 
-notesRouter.post('/', (req, res, next) => {
+notesRouter.post('/', async (req, res, next) => {
   const body = req.body;
   console.log(body);
 
-  if (!body.content) {
-    return res.status(400).json({
-      error: 'content missing',
-    });
-  }
+  // if (!body.content) {
+  //   return res.status(400).json({
+  //     error: 'content missing',
+  //   });
+  // }
 
   const note = new Note({
     content: body.content,
-    important: Boolean(body.important),
+    important: Boolean(body.important) || false,
   });
 
-  note
-    .save()
-    .then((savedNote) => {
-      res.json(savedNote);
-    })
-    .catch((error) => next(error));
+  try {
+    const savedNote = await note.save();
+    res.status(201).json(savedNote);
+  } catch (error) {
+    next(error);
+  }
 });
 
-notesRouter.get('/:id', (req, res, next) => {
-  Note.findById(req.params.id)
-    .then((note) => {
-      if (note) {
-        res.json(note);
-      } else {
-        res.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
+notesRouter.get('/:id', async (req, res, next) => {
+  try {
+    const note = await Note.findById(req.params.id);
+    if (note) {
+      res.json(note);
+    } else {
+      res.status(404).end();
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 notesRouter.put('/:id', (req, res, next) => {
@@ -61,12 +62,13 @@ notesRouter.put('/:id', (req, res, next) => {
     .catch((error) => next(error));
 });
 
-notesRouter.delete('/:id', (req, res, next) => {
-  Note.findByIdAndDelete(req.params.id)
-    .then(() => {
-      res.status(204).end();
-    })
-    .catch((error) => next(error));
+notesRouter.delete('/:id', async (req, res, next) => {
+  try {
+    await Note.findByIdAndDelete(req.params.id);
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = notesRouter;
